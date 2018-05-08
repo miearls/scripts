357ec1bf5a5af96571d41c07533af1c59786792a
Michael Earls <michael@michaelearls.com>
2018-05-08 12:00:05 -0400

    First release of script

---
+++ delete_older_file.py
@@ -0,0 +1,21 @@
+import os
+import time
+import sys
+
+if len(sys.argv) != 2:
+    print "usage", sys.argv[0], "To use this script you must defined the directory to scan: script <DIR>"
+    sys.exit(1)
+
+workdir = sys.argv[1]
+
+now = time.time()
+old = now - 300 * 24 * 60 * 60
+
+for f in os.listdir(workdir):
+    path = os.path.join(workdir, f)
+    if os.path.isfile(path):
+        stat = os.stat(path)
+        if stat.st_ctime < old:
+            print "removing: ", path
+            # os.remove(path) # uncomment when you will sure :)
+
